b35accfbb4f76ff78abdabc0e6cfe68cdc9b34d7
Martin Kleiven <m.klei@live.no>
2026-07-22 13:07:39 +0200

    Redirect blifrivillig domain to volunteer page (#77)

--- next.config.ts
+++ next.config.ts
@@ -44,6 +44,17 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "(?:www\\.)?blifrivillig\\.no",
+          },
+        ],
+        destination: "https://www.samfunnetibergen.no/blifrivillig",
+        permanent: true,
+      },
+      {
         source: "/program",
         destination: "/nb/arrangementer",
         permanent: true,
